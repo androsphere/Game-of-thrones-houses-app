8a14ca9f3f95d9d53ad97e87f9e615db0f98a0cd
Andrew MacIsaac <amacisaac95@gmail.com>
2023-03-09 11:58:21 -0700

    House names and locations working

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 function fetchHouses() {
-    // To pass the tests, don't forget to return your fetch!
-    fetch("https://anapioficeandfire.com/api/houses")
+
+  for(let i=1; i<= 9;){
+    fetch(`https://anapioficeandfire.com/api/houses?page=${i}&pageSize=50`)
     .then((resp) => resp.json())
-    .then((json) => renderHouses(json));
-    return fetch("https://anapioficeandfire.com/api/houses")
+    .then((json) => renderHouses(json))
+    i++;
+  }
+    
   }
   
   function renderHouses(houses) {
@@ -12,10 +15,14 @@ function fetchHouses() {
       const h2 = document.createElement('h2');
       h2.innerHTML = house.name;
       main.appendChild(h2);
+      const h3 = document.createElement('h3');
+      h3.innerHTML = 'Region: ' + house.region;
+      main.appendChild(h3);
     });
   }
   
   document.addEventListener('DOMContentLoaded', function() {
     fetchHouses();
+
   });
   

e5f378daa56359ff5a2e0b5fadae82d86b7af831
Andrew MacIsaac <amacisaac95@gmail.com>
2023-03-09 16:07:15 -0700

    Kinda works

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
 function fetchHouses() {
-
-  for(let i=1; i<= 9;){
+  for (let i = 1; i <= 9; i++) {
     fetch(`https://anapioficeandfire.com/api/houses?page=${i}&pageSize=50`)
-    .then((resp) => resp.json())
-    .then((json) => renderHouses(json))
-    i++;
+      .then(resp => resp.json())
+      .then(json => renderHouses(json));
   }
-    
-  }
-  
-  function renderHouses(houses) {
-    const main = document.querySelector('main');
-    houses.forEach(house => {
-      const h2 = document.createElement('h2');
-      h2.innerHTML = house.name;
-      main.appendChild(h2);
-      const h3 = document.createElement('h3');
-      h3.innerHTML = 'Region: ' + house.region;
-      main.appendChild(h3);
-    });
-  }
-  
-  document.addEventListener('DOMContentLoaded', function() {
-    fetchHouses();
+}
 
+function renderHouses(houses) {
+  const main = document.querySelector('main');
+  houses.forEach((house, index) => {
+    const button = document.createElement('button');
+    button.textContent = house.name;
+    button.className = "houses";
+    button.id = index + 1;
+    main.appendChild(button);
+    const detailsDiv = document.createElement('div');
+    detailsDiv.setAttribute("hidden", true);
+    detailsDiv.className = "details";
+    main.appendChild(detailsDiv);
+    button.addEventListener("click", () => {
+      const details = document.querySelectorAll(".details")[index];
+      details.toggleAttribute("hidden");
+    });
+    renderDetails(detailsDiv, house.region, "Region: ");
+    renderDetails(detailsDiv, house.coatOfArms, "Coat of Arms: ");
+    renderDetails(detailsDiv, house.words, "Words: ");
+    renderDetails(detailsDiv, house.titles, "Titles: ");
+    renderDetails(detailsDiv, house.seats, "Seats: ");
+    renderDetails(detailsDiv, house.ancestralWeapons, "Ancestral Weapons: ");
   });
-  
+}
+
+function renderDetails(detailsDiv, input, detailName) {
+  if (input.length > 3) {
+    const details = document.createElement('p');
+    details.textContent = `${detailName}${input}`;
+    detailsDiv.appendChild(details);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', fetchHouses);

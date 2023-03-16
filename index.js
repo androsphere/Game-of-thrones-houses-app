5b294955edd516aca414f24cce5a8b33e461cd54
Andrew MacIsaac <amacisaac95@gmail.com>
2023-03-15 19:32:31 -0600

    Functions made smaller for debugging and easier maintenance

--- index.js
+++ index.js
@@ -14,62 +14,50 @@ function renderHouses(houses) {
     button.className = "houses";
     button.id = index + 1;
     main.appendChild(button);
-
-    
     const detailsDiv = document.createElement('div');
     detailsDiv.setAttribute("hidden", true);
     detailsDiv.className = "details";
     main.appendChild(detailsDiv);
-
-    
     button.addEventListener("click", () => {
       const details = document.querySelectorAll(".details")[index];
       details.toggleAttribute("hidden");
     });
-
-
     renderDetails(detailsDiv, house.region, "Region: ");
     renderDetails(detailsDiv, house.coatOfArms, "Coat of Arms: ");
     renderDetails(detailsDiv, house.words, "Words: ");
     renderDetails(detailsDiv, house.titles, "Titles: ");
     renderDetails(detailsDiv, house.seats, "Seats: ");
     renderDetails(detailsDiv, house.ancestralWeapons, "Ancestral Weapons: ");
-
-    const form = document.createElement('form');
-    const inputComment = document.createElement('input');
-    inputComment.setAttribute('type', 'text');
-    inputComment.setAttribute('placeholder', 'Add a Note');
-    form.appendChild(inputComment);
-    const btnSubmit = document.createElement('button');
-    btnSubmit.setAttribute('type', 'submit');
-    btnSubmit.textContent = 'Submit';
-    form.appendChild(btnSubmit);
-    detailsDiv.appendChild(form);
-
-    
-    form.addEventListener('submit', (event) => {
-      event.preventDefault(); 
-      const comment = inputComment.value.trim(); 
-      if (comment) {
-        const commentElement = document.createElement('p'); 
-        commentElement.textContent = comment;
-        detailsDiv.appendChild(commentElement); 
-        inputComment.value = ''; 
-      }
-    });
+    addComment(detailsDiv);
   });
 }
 
-function renderDetails(detailsDiv, input, detailName) {  
-  if(input === ""){
+function renderDetails(detailsDiv, input, detailName) {
+  if (input !== '') {
+    const details = document.createElement('p');
+    details.textContent = `${detailName}${input}`;
+    detailsDiv.appendChild(details);
+  }
+}
 
-  }
-  else{
-  const details = document.createElement('p');
-  details.textContent = `${detailName}${input}`;
-  detailsDiv.appendChild(details);  
-  
-  }
+function addComment(detailsDiv) {
+  const form = document.createElement('form');
+  const inputComment = document.createElement('input');
+  inputComment.setAttribute('type', 'text');
+  inputComment.setAttribute('placeholder', 'Leave a comment');
+  form.appendChild(inputComment);
+  const btnSubmit = document.createElement('button');
+  btnSubmit.setAttribute('type', 'submit');
+  btnSubmit.textContent = 'Submit';
+  form.appendChild(btnSubmit);
+  detailsDiv.appendChild(form);
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const comment = document.createElement('p');
+    comment.textContent = inputComment.value;
+    detailsDiv.appendChild(comment);
+    inputComment.value = '';
+  });
 }
 
 document.addEventListener('DOMContentLoaded', fetchHouses);
